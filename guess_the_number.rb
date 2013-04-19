0035682176fffc71d6239ff9865f362091caf071
Casey Dierking <hnwebdesign5@gmail.com>
2013-04-19 11:58:03 +0700

    Added in comments

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,16 +1,17 @@
 def guess_the_number
+	#Generates a random number between 1-100 and then prompts the user for their guess.
 	num = rand(100)
 	puts "Please guess the number!"
 	guess = gets.to_i
 	
-	
+	# This sees if the randomly generated number matches the guess, and acts accordingly.
 	while num != guess
 		puts "Sorry! Your number: #{guess} is too low" if num > guess
 		puts "Sorry! Your number: #{guess} is too high" if num < guess
 		puts "Give it another shot?"
 		guess = gets.to_i
 	end
-
+	# If the numbers match up, you win!
 	if num == guess
 		puts "Congratulations! You've guessed correctly! The number was #{num}"
 		exit
